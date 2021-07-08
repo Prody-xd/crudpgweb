d4f674b0a3693561ec5e5fc6ff102f070db9850b
Diego <Blainesxd@gmail.com>
2021-07-07 21:30:46 -0400

    Subiendo mod:avance api sin error

--- miprimerdjango/core/models.py
+++ miprimerdjango/core/models.py
@@ -23,8 +23,8 @@ class Vehiculo(models.Model):
 class mecanicos (models.Model):
     nombre = models.CharField(max_length=250, verbose_name='Nombre')
     telefono = models.CharField(max_length=250, verbose_name='Telefono')
-    email = models.CharField(max_length=250, verbose_name='Telefono')
+    email = models.CharField(max_length=250, verbose_name='Email')
     especialidad = models.CharField(max_length=250, verbose_name='Especialidad')
-    tiempo_exp = models.CharField(max_length=250, verbose_name='TiempoExp')
+    tiempo_exp = models.CharField(max_length=250, verbose_name='Tiempo de Experiencia')
     def __str__(self):
         return self.nombre
--- miprimerdjango/rest_api/serializers.py
+++ miprimerdjango/rest_api/serializers.py
@@ -2,7 +2,7 @@ from django.db.models import fields
 from rest_framework import serializers
 from core.models import mecanicos
 
-class mecanicosSerializers(serializers,serializers.ModelSerializer):
+class mecanicosSerializers(serializers.ModelSerializer):
     class Meta:
         model = mecanicos
-        fields['nombre','telefono','email','especialidad','tiempo_exp']
+        fields = ['nombre','telefono','email','especialidad','tiempo_exp']
